71051151a20ef790d64418255a7d97f6c602f437
Akiomi Kamakura <akiomik@gmail.com>
2013-11-09 23:58:44 +0900

    modified plugin templates

--- src/main/g8/src/main/scala/$organization__packaged$/$name__Camel$.scala
+++ src/main/g8/src/main/scala/$organization__packaged$/$name__Camel$.scala
@@ -3,14 +3,14 @@ package $organization$
 import sbt._
 import Keys._
 
-class $name;format="Camel"$ extends Plugin {
+object $name;format="Camel"$ extends Plugin {
   override lazy val settings = Seq(
     commands ++= Seq(
       sample
     )
   )
 
-  lazy val sample = Command.command("sample-command") { state =>
+  lazy val sample = Command.command("sampleCommand") { state =>
     println("Hello SBT World!")
     state
   }
